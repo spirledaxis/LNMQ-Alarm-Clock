a67838cd18367ddab9b075a06e73d190e57659de
spirledaxis <spirledaxis@gmail.com>
2025-09-12 18:12:49 -0700

    format + rename + remove unused stuff

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ motor_r = 18
 motor_pwm_freq = 20_000
 motor_min_pwm = 37_000
 motor = Motor(motor_l, motor_r, motor_pwm_freq, motor_min_pwm)
+
 # dfplayer
 rx = 17
 tx = 16
@@ -16,6 +17,7 @@ transistor = 21
 busy = 20
 uarto_channel_df = 0
 speaker = DFPlayer(uarto_channel_df, tx, rx, busy, transistor)
+
 # display
 """
 pins from gnd-cs
@@ -33,15 +35,13 @@ res = 13
 dc = 12
 cs = 11
 spi_channel_disp = 1
-
 spi = SPI(spi_channel_disp, baudrate=10_000_000, sck=Pin(sck), mosi=Pin(sda))
 display = Display(spi, dc=Pin(dc), cs=Pin(cs), rst=Pin(res),
                   offscreen_warnings=False, flip=True)
 
 # inputs
 snd_fx_l = 8  # these are also paired
-# right wire fell off, idc enough to solder it on. Thus, use the left one
-snd_fx_r = 2
+snd_fx_r = 2 # right wire fell off, idc enough to solder it on. Thus, use the left one
 snze_l = 4  # the snze buttons seems to be paired
 snze_r = 3  # so, you can use either (but not both!)
 alm_set = 0
@@ -57,9 +57,7 @@ messenger_cycle_time_s = 30
 messenger_icon_cycle_time_s = 10
 alarm_timeout_min = 5
 bsod_timeout_s = 15
-display_timer = Neotimer(display_timeout_min * 60_000)
 
-# other
-ip = '192.168.1.51'
+# ips
 server_ip = '192.168.1.21'
 server_port = 8080
