053d188e199a174b0ea1eaaabc98a210fd0df0dc
spirledaxis <spirledaxis@gmail.com>
2025-10-18 10:03:21 -0700

    Add headlights and remove enabled varible
    Mainloop now controls if update is ran or not

--- alarm.py
+++ alarm.py
@@ -3,8 +3,10 @@ from lib.neotimer import Neotimer
 import json
 from displaystates import aliases
 import motd_parser
+
+
 class Alarm:
-    def __init__(self, timeout_s, motor, speaker, switch):
+    def __init__(self, timeout_s, motor, headlights, speaker, switch):
         """use military time for the hour. """
         with open('alarm.json', 'r') as f:
             # print(f.read())
@@ -16,32 +18,26 @@ class Alarm:
 
         self.hour = timeutils.to_military_time(alarm_hour, alarm_ampm)
         self.timeout_timer = Neotimer(timeout_s * 1000)
-        self.enabled = switch.get_state()
         self.switch = switch
         self.is_active = False
         self.locked = False  # used so we don't call fire for the entire minute
         self.motor = motor
+        self.headlights = headlights
         self.motor.set_movement_by_ringtone(self.ringtone)
         self.speaker = speaker
         self.speaker_state_timer = Neotimer(1000)
         self.original_ringtone = self.ringtone
-        
 
     def update(self, now, home):
         """
         Args:
             now: a rtc tuple"""
         #(year, month, mday, hour, minute, second, weekday, yearday)
-        self.switch.update()
-        if self.switch.get_state():
-            self.enabled = True
-        else:
-            self.enabled = False
 
         now_hour = now[4]
         now_minute = now[5]
         #print(self.hour, self.minute, self.locked, self.enabled)
-        if now_hour == self.hour and now_minute == self.minute and not self.locked and self.enabled:
+        if now_hour == self.hour and now_minute == self.minute and not self.locked:
             self.fire(now, home)
             print("firing")
 
@@ -58,13 +54,13 @@ class Alarm:
         if self.is_active:
             if not self.speaker.queryBusy() and self.speaker_state_timer.finished():
                 print("going at it again")
-                #self.motor.stop()  # stop the motor in the case that the movement is longer than the audio
+                # self.motor.stop()  # stop the motor in the case that the movement is longer than the audio
                 # Can't use this because it introduces delay which cooks sync
                 self.speaker.playTrack(1, self.ringtone)
                 self.motor.start()
                 self.speaker_state_timer.restart()
 
-            #self.motor.do_movement() #moved to another thread
+            # self.motor.do_movement() #moved to another thread
 
     def fire(self, now, home):
         if self.locked:
@@ -91,6 +87,7 @@ class Alarm:
         self.speaker.playTrack(1, self.ringtone)
         self.motor.start()
         self.motor.set_movement_by_ringtone(self.ringtone)
+        self.headlights.start(self.ringtone)
         self.timeout_timer.start()
         self.speaker_state_timer.start()
 
@@ -101,6 +98,7 @@ class Alarm:
 
         self.speaker.pause()
         self.motor.stop()
+        self.headlights.stop()
         self.timeout_timer.reset()
         self.is_active = False
         self.ringtone = self.original_ringtone
