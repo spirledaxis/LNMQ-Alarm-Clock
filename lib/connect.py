5f8376e0dfe6ee84e49e8752a39e8378de67d0f1
spirledaxis <spirledaxis@gmail.com>
2025-10-18 10:10:38 -0700

    add test code

--- lib/connect.py
+++ lib/connect.py
@@ -14,7 +14,11 @@ def do_connect():
     print('network config:', sta_if.ipconfig('addr4'))
     return sta_if
 
+
 def check_connection():
     sta_if = network.WLAN(network.WLAN.IF_STA)
     return sta_if.isconnected()
 
+
+if __name__ == '__main__':
+    do_connect()
