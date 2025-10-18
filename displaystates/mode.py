7896a6e1234dd325237c3821f66b4193c52d77ae
spirledaxis <spirledaxis@gmail.com>
2025-10-18 10:10:19 -0700

    add alarm_active to DisplayManager
    Makes the alarm active variable more accesible

--- displaystates/mode.py
+++ displaystates/mode.py
@@ -5,7 +5,7 @@ from lib.xglcd_font import XglcdFont
 import json
 from lib.neotimer import Neotimer
 from utime import ticks_ms  # type: ignore
-from machine import Pin #type: ignore
+from machine import Pin  # type: ignore
 
 print("loading fonts...")
 timefont = XglcdFont('Proxy24x31.bin', 24, 31)
@@ -21,6 +21,9 @@ class DisplayManager:
         self.display_timer = Neotimer(config.display_timeout_min * 60_000)
         self.display_timer.start()
         self.switch = Switch(config.switch)
+        with open('alarm.json', 'r') as f:
+            alarmdata = json.load(f)
+            self.alarm_active = alarmdata['is_active']
 
     def set_active_state(self, name):
         print("called activiate state")
@@ -31,13 +34,12 @@ class DisplayManager:
                 self.current_state_obj = display_state
             else:
                 display_state.active = False
-    
+
         if self.current_state_obj.name == 'message_reader':
             self.display_timer = Neotimer(
                 config.display_messenger_timeout_min * 60_000)
         else:
             self.display_timer = Neotimer(config.display_timeout_min * 60_000)
-        
 
         self.display_timer.start()
 
@@ -60,6 +62,9 @@ class DisplayManager:
 
         self.switch.update()
 
+        if self.current_state == aliases.set_alarm:
+            self.alarm_active = self.current_state_obj.alarm_active
+
 
 class DisplayState:
     def __init__(self, buttonmap, name, display_manager: DisplayManager):
@@ -68,6 +73,3 @@ class DisplayState:
         self.active = False
         self.button_map = buttonmap
         self.display_manager = display_manager
-
-
-
