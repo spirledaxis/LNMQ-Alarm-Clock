c2ea9d5001551d149be2ddd61dcfd18db4c73c04
spirledaxis <spirledaxis@gmail.com>
2025-10-24 12:07:17 -0700

    do_connect retries upon failure

--- lib/connect.py
+++ lib/connect.py
@@ -1,17 +1,27 @@
 from wificreds import name, password
 import network  # type: ignore
-
+import time
 
 def do_connect():
-    print('do_connected called')
-    sta_if = network.WLAN(network.WLAN.IF_STA)
+    print('do_connect called')
+    sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('Connecting to network...')
         sta_if.active(True)
         sta_if.connect(name, password)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ipconfig('addr4'))
+        for _ in range(20):
+            if sta_if.isconnected():
+                break
+            time.sleep(0.5)
+    
+    ip, subnet, gateway, dns = sta_if.ifconfig()
+    print('Network config:', (ip, subnet, gateway, dns))
+
+    if ip == "0.0.0.0":
+        print("Detected bad IP, retrying...")
+        time.sleep(5)
+        do_connect()
+
     return sta_if
 
 
