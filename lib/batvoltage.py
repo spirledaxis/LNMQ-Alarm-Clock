db4b529b7575e2a6243565165afc23348e126367
spirledaxis <spirledaxis@gmail.com>
2025-10-29 17:42:27 -0700

    rename to batstats.py

--- lib/batvoltage.py
+++
@@ -1,8 +0,0 @@
-from machine import ADC, Pin
-import config
-adc = ADC(Pin(config.bat_adc))
-
-def read_bat_voltage():
-    reading = adc.read_u16()  # 16-bit value (0-65535)
-    voltage = 2 * reading * 3.3 / 65535
-    return voltage
