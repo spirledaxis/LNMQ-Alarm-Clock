4098dbc4617061aa6ff731172051c76de9d88547
spirledaxis <spirledaxis@gmail.com>
2025-11-05 16:35:10 -0800

    fonts are in displaystates

--- displaystates/fonts.py
+++ displaystates/fonts.py
@@ -1,6 +1,6 @@
 from lib import XglcdFont
 
 print("loading fonts...")
-timefont = XglcdFont('Proxy24x31.bin', 24, 31)
-bally = XglcdFont('Bally7x9.bin', 7, 9)
-bally_mini = XglcdFont('Bally5x8.bin', 5, 8)
+timefont = XglcdFont('displaystates/Proxy24x31.bin', 24, 31)
+bally = XglcdFont('displaystates/Bally7x9.bin', 7, 9)
+bally_mini = XglcdFont('displaystates/Bally5x8.bin', 5, 8)
